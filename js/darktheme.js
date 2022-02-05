746c0c445c6f836101958209e9db29a113fd93b2
AminNdr <amin.korya@gmail.com>
2022-02-05 17:20:10 +0330

    some minor changes

--- js/darktheme.js
+++ js/darktheme.js
@@ -1,57 +1,32 @@
-// let areas = document.querySelectorAll("section.themeArea");
-// let themeSelector = document.getElementById('themeSelector');
-// let checkedRadio = 'light';
-
-
-
-
-
-// themeSelector.addEventListener('change', function () {
-//     checkedRadio = document.querySelector('input[name=theme]:checked').value;
-//     changeThemeColor(checkedRadio);
-// })
-
-// function changeThemeColor(color) {
-//     if (color == 'dark'){
-//         for (let i = 0; i < areas.length; i++) {
-//     areas[i].style.backgroundColor = "#0e1015";
-//     areas[i].style.color = "#fff";
-// }
-//     }else{
-//         for (let i = 0; i < areas.length; i++) {
-//             areas[i].style.backgroundColor = "#fff";
-//             areas[i].style.color = "#000";
-//         }
-//     }
-
-// }
-
-
-
-/************************************/
-
 let areas = document.querySelectorAll("section.themeArea");
 let lightAreas = document.querySelectorAll('section.lightArea');
-// let darkAreas = document.querySelectorAll('section.darkArea');
+let darkAreas = document.querySelectorAll('section.darkArea');
 let themeCheckBox = document.getElementById('themeCheckBox');
 
 
 function changeTheme() {
 
-        
-        for(let i = 0 ; i < lightAreas.length ; i++){
-           
-            if (themeCheckBox.checked == true) {
 
+    for (let i = 0; i < lightAreas.length; i++) {
 
+        if (themeCheckBox.checked == true) {
             lightAreas[i].style.backgroundColor = '#0e1015';
+            // lightAreas[i].style.color = '#fff';
+          
+        } else {
+            lightAreas[i].style.backgroundColor = '#fff';
             
-           
-            }   else {
-                lightAreas[i].style.backgroundColor = '#fff';
-            }
-       
-        }  
+        }
+
+    }
+
+    for (let i = 0; i < darkAreas.length; i++) {
+        if (themeCheckBox.checked == true){
+            darkAreas[i].style.backgroundColor = '#fff';
+        } else {
+            darkAreas[i].style.backgroundColor = '#0e1015';
+        }
+    }
 }
 
 
--- js/readMore.js
+++ js/readMore.js
@@ -1,74 +1,3 @@
-// let seoBtn = document.getElementById('seo-readmore-btn');
-// let seoCol = document.getElementById('seoCol');
-// let seoMoreDesc = document.getElementById('seo-moreDesc');
-
-
-
-// function hideBtn() {
-//     seoBtn.style.visibility = 'hidden';
-// }
-
-
-// function showDesc() {
-
-//     seoMoreDesc.style.visibility = "visible";
-//     // seoMoreDesc.style.opacity = 1;
-
-//     // var show = setInterval(function () {
-//     //     let opc = seoMoreDesc.style.opacity;
-//     //     let newOpc = Number.parseFloat(opc) + 0.2;
-//     //     console.log(typeof(opc) + ' ' + opc);
-        
-//     //     seoMoreDesc.style.opacity = newOpc;
-//     // }, 1000)
-
-//     seoMoreDesc.style.height = "100px";
-//     setTimeout(()=>{
-//         seoMoreDesc.style.opacity = 1;
-//     },1000)
-// }
-
-// const btnHandler = (event) =>{
-// console.log(event.target)
-// }
-// document.getElementById('seo-readmore-btn').addEventListener('click',btnHandler);
-// function incHeight() {
-
-//     var limiter = 0;
-//     var inc = setInterval(function () {
-//         let height = seoCol.offsetHeight;
-//         let newHeight = height + 1;
-//         limiter++;
-//         seoCol.style.height = newHeight + 'px';
-
-
-//         if (limiter > 100) {
-//             clearInterval(inc);
-//         }
-//     }, 10);
-
-// }
-
-
-
-
-
-
-// seoBtn.addEventListener('click', hideBtn);
-// seoBtn.addEventListener('click', incHeight);
-
-
-
-
-
-
-
-
-
-/* event deligation */ 
-
-
-
 btns = document.querySelectorAll('.readMorebtn');
 
 function revealPara (event){
@@ -76,6 +5,7 @@ function revealPara (event){
     let para = btn.previousElementSibling;
     para.style.height = '100px';
     btn.style.opacity = '0';
+    btn.style.visibility = 'hidden' ;
 
 }
 
